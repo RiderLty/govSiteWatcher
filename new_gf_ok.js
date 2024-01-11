cce93887058849aece526046006ef09cf339c092
DriverLin <lty6531600@gmail.com>
2024-01-11 16:43:39 +0800

    update

--- new_gf_ok.js
+++ new_gf_ok.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         读取本地XLSX文件并转换为二维数组
+// @name         网站更新检测
 // @namespace    http://tampermonkey.net/
 // @version      1.1
-// @description  读取本地XLSX文件并将表格中第一个工作簿转换为二维数组，然后打印在控制台
+// @description  网站更新检测
 // @author       You
 // @match        https://59.203.54.81:8012/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=54.81
@@ -12,45 +12,33 @@
 
 
 //export tree=================================
-const sleep = async (s) => new Promise(resolve => setTimeout(resolve, s * 1000));
+const sleep = async s => new Promise(resolve => setTimeout(resolve, s * 1000));
 
-const listChildren = (root) => {//root为li 且class为levelX X是数字
-    const level = Number(root.className.slice(5))
-    return root.querySelectorAll(`li.level${level + 1}`)
-}
+const listChildren = (root) => [...root.querySelectorAll(`li.level${Number(root.className.slice(5)) + 1}`)]
 
 const dfsVisit = async (node) => {
     const span = node.querySelector("span")
-
     if (span.className.indexOf("docu") === -1 && span.className.indexOf("open") === -1) {
         span.click()
-        let count = 0
-        while (listChildren(node).length === 0 && count <= 10) {//1秒超时 空栏目
+        for (let count = 0; listChildren(node).length === 0 && count <= 10; count++) {
             await sleep(0.1)
-            count += 1
         }
     }
-
     const nodeName = (node.querySelector("a > span.node_name")).innerText
     const uid = node.querySelector("a").title.match(/\d+/g)[0]
     const display = `${nodeName}][ID:${uid}`
-    const sonList = await Promise.all([...listChildren(node)].map(async son => await dfsVisit(son)));
+    const sonList = await Promise.all(listChildren(node).map(async son => await dfsVisit(son)));
     return sonList.length === 0 ? display : { [display]: sonList }
 }
 
-const linePrint = (obj, head) => {
-    if (typeof obj === typeof "str") {
-        return [[...head, obj]]
-    } else {
-        return Object.keys(obj).flatMap(key => obj[key].flatMap(son => linePrint(son, [...head, key])));
-    }
-}
+const linePrint = (obj, head) => typeof obj === typeof "str" ?
+    [[...head, obj]] :
+    Object.keys(obj).flatMap(key => obj[key].flatMap(son => linePrint(son, [...head, key])))
 
 const dfs = async () => {
     const root = document.querySelectorAll("li.level0")
     const elemFrost = await Promise.all([...root].map(elem => dfsVisit(elem)))
-    const listVer = elemFrost.map(tree => linePrint(tree, [])).flat()
-    return listVer
+    return elemFrost.map(tree => linePrint(tree, [])).flat()
 }
 
 //==================================================================================
@@ -67,7 +55,6 @@ const waitAndClick = async (path, interval) => {
         }, interval);
     });
 };
-
 
 const readLocalExcel = async (url) => {
     return new Promise((resolve, reject) => {
@@ -91,19 +78,39 @@ const readLocalExcel = async (url) => {
     })
 }
 
+const exportExcel = async (data, name) => {
+    const workbook = XLSX.utils.json_to_sheet(data);
+    const workbookOutput = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbookOutput, workbook, "Sheet1");
+    const excelBuffer = XLSX.write(workbookOutput, {
+        bookType: "xlsx",
+        type: "array",
+    });
+    const blob = new Blob([excelBuffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = name;
+    link.style.display = "none";
+    document.body.appendChild(link);
+    link.click();
+    URL.revokeObjectURL(url);
+    document.body.removeChild(link);
+}
+
 const dataPostFetch = async (url, body) => {
     const resp = await fetch(url, {
-        "headers": {
+        headers: {
             "accept": "application/json, text/javascript, */*; q=0.01",
             "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5",
             "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
             "x-requested-with": "XMLHttpRequest"
         },
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": body,
-        "method": "POST",
-        "mode": "cors",
-        "credentials": "include"
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: body,
+        method: "POST",
+        mode: "cors",
+        credentials: "include"
     })
     const rawData = await resp.json()
     const sortedDate = rawData.data.sort((a, b) => new Date(b.publishDate) - new Date(a.publishDate))//按日期排列，确保最新，因为可能有固定的
@@ -139,17 +146,19 @@ const printResult = (info, title, distance, daysRemain) => {
     ].join("\n")
     if (daysRemain <= 0) {
         console.error(infoText)
-    } else if (daysRemain <= 2) {
-        console.warn(infoText)
-    } else {
-        console.log(infoText)
+        return
     }
+    if (daysRemain <= 2) {
+        console.warn(infoText)
+        return
+    }
+    console.log(infoText)
 }
 
-const calcResult = (result) => {
+const calcResult = (__result) => {
+    const result = __result.filter(val => val)
     const dateMap = {}
     const limitMap = {}
-
     for (let [item, title, date] of result) {
         if (item === null) {
 
@@ -169,12 +178,16 @@ const calcResult = (result) => {
         printResult(key, "标题略", distance, daysRemain)
     }
 
-    //计算一般类型
+    //计算一般类型and额外天数要求
     for (let [item, title, date] of result) {
         if (item && typeof item["更新要求"] === typeof 0) {
             const distance = Number.parseInt((new Date() - new Date(date)) / 86400000)
             const daysRemain = item["更新要求"] - distance
-            // console.log(`${item["栏目名称"]}\n距今${distance}天\t剩余${daysRemain}天\n===================================`)
+            printResult(item, title, distance, daysRemain)
+        }
+        else if (item && String(item["更新要求"]).indexOf("[") !== -1 && String(item["更新要求"]).indexOf("]") !== -1) {
+            const distance = Number.parseInt((new Date() - new Date(date)) / 86400000)
+            const daysRemain = Number(String(item["更新要求"]).split("[")[1].split("]")[0]) - distance
             printResult(item, title, distance, daysRemain)
         }
     }
@@ -211,14 +224,29 @@ const mainFunction = async () => {
     lock = true
     const lmList = await dfs()
     const result = []
-    for (let lm of lmList) {
+    const exportDataWhenMiss = []
+    let missFlag = false
+
+    await Promise.all(lmList.map(async (lm, index) => {
+        const [name, id] = lm[lm.length - 1].split("][ID:")
+        const head = lm[0].split("][ID:")[0]
+        const path = lm.map(name => name.split("][ID:")[0]).join(" > ")
+        const block = {
+            "完整地址": path,
+            "栏目名称": name,
+            "更新要求": null,
+            "责任科室": null,
+            "详细说明": null,
+        }
         try {
-            const [name, id] = lm[lm.length - 1].split("][ID:")
-            const head = lm[0].split("][ID:")[0]
-            const path = lm.map(name => name.split("][ID:")[0]).join(" > ")
             if (excelPathMap[path]) {
+                block["更新要求"] = excelPathMap[path]["更新要求"]
+                block["责任科室"] = excelPathMap[path]["责任科室"]
+                block["详细说明"] = excelPathMap[path]["详细说明"]
+                // exportDataWhenMiss.push(block)
+                exportDataWhenMiss[index] = block
                 if (excelPathMap[path]["更新要求"] === -1) {
-                    continue
+                    return
                 } else {
                     let fetchResult = null
                     if (head === "滁州市人民政府办公室") {//市政府网站
@@ -229,15 +257,28 @@ const mainFunction = async () => {
                         fetchResult = await getLatestNRGL(id)
                     }
                     console.log(fetchResult)
-                    result.push([excelPathMap[path] , ...fetchResult])
+                    // result.push([excelPathMap[path], ...fetchResult])
+                    result[index] = [excelPathMap[path], ...fetchResult]
                 }
             } else {
-                console.log("miss!", name, path)
-                continue
+                console.warn("miss! : ", path)
+                block["更新要求"] = "NULL"
+                block["责任科室"] = "NULL"
+                block["详细说明"] = "NULL"
+                // exportDataWhenMiss.push(block)
+                exportDataWhenMiss[index] = block
+                missFlag = true
+                return
             }
         } catch (err) {
-            console.log(err)
+            console.error(path, "\n", err)
         }
+    }))
+    // for (let lm of lmList) {
+
+    // }
+    if (missFlag) {
+        exportExcel(exportDataWhenMiss, "栏目缺失")
     }
     calcResult(result)
     lock = false
